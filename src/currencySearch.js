1f7d5059631ef47e9a38756f60132575db5383c6
ophelia swen <ophelia.swen@gmail.com>
2020-09-22 08:37:51 -0700

    use console log as instructor said

--- src/currencySearch.js
+++ src/currencySearch.js
@@ -1,14 +1,17 @@
 export default class CurrencySearch {  
-  static getCurrency(type) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD/${type}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
+  static getCurrency(country) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD/${country}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
         }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/main.js
+++ src/main.js
@@ -8,26 +8,39 @@ function clearFields() {
   $('#currency-type').val("");
   $('.showErrors').text("");
   $('.showCurrency').text("");
+  $('.showNotify').text("");
 }
-
 function getElements(response) {
-  if (response.main) {
-    $('.showCurrency').text(`The currency exchange for ${response.USD} is ${response.conversion_rates[0]}.`);
-  } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
-    $('.showNotify').text('not a valid currency');
+  console.log(response);
+  let selection = country;
+    if(selection === "AED") {
+      $('#showCurrency').text('selection * `${response.conversion_rates.AED}`');
+    } else if (selection === "ARS") {
+      $('#showCurrency').text('selection * `${response.conversion_rates.ARS}`');
+    } else if (selection === "AUD") {
+      $('#showCurrency').text('selection * `${response.conversion_rates.AUD}`');
+    } else if (selection === "BGN") {
+      $('#showCurrency').text('selection * `${response.conversion_rates.BGN}`');
+    } else if (selection === "BRL") {
+      $('#showCurrency').text('selection * `${response.conversion_rates.BRL}`');
+    } else if (selection === "BSD") {
+      $('#showCurrency').text('selection * `${response.conversion_rates.BSD}`');
+    } else if (selection === "HJK") {
+      $(".showNotify").text(`Alert: Currency not in database`);
+    } else {
+      $(".showErrors").text(`Error Alert: ${response.message}`);
+    }
   }
-}
 
 $(document).ready(function() {
   $('#conversionRate').click(function() {
-    let type = $('#conversionRate').val();
+    event.preventDefault();
+    let country = $('#currency-type').val();
     clearFields();
-    CurrencySearch.getCurrency(type)
-      .then(function(response) {
-        getElements(response);
-      });
+    let promise = CurrencySearch.getCurrency(country);
+    promise.then(function(response) {
+      getElements(response);
+    });
   });
 });
-
-
+  
